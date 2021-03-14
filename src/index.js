0c1eba9314cb7c22da63af5e79ac17e970bbcbb8
Dunzer <dunzer.gunt@gmail.com>
2021-03-14 14:23:28 +0400

    fix: improve algorithms

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-
 exports.min = function min (array) {
-  if (array.length === 0 || !array) {
+  if (!array || array.length === 0) {
     return 0;
-  } else {
+  } 
     return Math.min(...array);
-  }
 }
 
 
 exports.max = function max (array) {
-  if (array.length === 0 || !array) {
+  if (!array || array.length === 0) {
     return 0;
-  } else {
+  } 
     return Math.max(...array);
-  }
 }
 
 
 exports.avg = function avg(array) {
-  if (array.length === 0 || !array) {
+  if (!array || array.length === 0) {
     return 0;
-  } else {
+  } 
     return array.reduce((sum, current) => sum + current, 0) / array.length;
-  }
 }
